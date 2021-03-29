7f273d7af1c4b99b5aff62f2eee4c537a7efd0f3
langan.kg Langan <langan.kg@gmail.com>
2021-03-29 16:36:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,10 +8,9 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
 
     if lowest_value = nil || value < lowest_value
-      lowest_key = value
+      lowest_value = value
       lowest_key = key
+      end
     end
-end
-lowest_key
-
+      lowest_key
 end
