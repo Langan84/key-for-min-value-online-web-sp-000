9b50c4265190d0d8f0eaf3d2fb4750f6a4fca1eb
langan.kg Langan <langan.kg@gmail.com>
2021-03-29 15:35:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,30 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+# 
 def key_for_min_value(name_hash)
 
+  min_key = nil 
+  min_value = nil
+
+  name_hash.collect do |item, value| 
+  
+  end
+
+  
+  #iterate over the hash 
+  #identify the lowest value 
+#return the lowest value 
+#return nul for an empty hash
+
+end
+**********
+
+birthday_kids.collect do |kids_name, age|
+  age
+end
+
+hash = {key1: "value1", key2: "value2"}
+ 
+hash.each do |key, value|
+  puts "#{key}: #{value}"
 end
